9de097e24b4b7bad21cf50e08de197d81a31fa32
onuki <rcpxg844@yahoo.co.jp>
2020-07-05 15:08:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AtCoder
+
+AtCoderでACされたコードを置いていく
---
+++ template/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+#import math
+#from decimal import *
+#from numba import njit
+
+#@njit
+def main():
+    (N, K) = map(int, input().split())
+    A = np.array(list(map(int, input().split())))
+    
+
+main()
